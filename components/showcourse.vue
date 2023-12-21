5c54428531fe79e317d9d871f22a845157cd7676
Glen Russell <154485100+guitaronblood666@users.noreply.github.com>
2023-12-22 01:34:20 +0900

    Update showcourse.vue

--- components/showcourse.vue
+++ components/showcourse.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="grid min-[768px]:grid-cols-[50%_50%] w-full">
     <div v-for="(item, index) in course" :key="index" class="grid ml-4">
       <label class="text-white w-full">{{ item.label }}</label>
       <label class="text-sky-900 ml-8 mr-4 w-4/5 h-[28px] rounded px-2">{{ item.value }}</label>
